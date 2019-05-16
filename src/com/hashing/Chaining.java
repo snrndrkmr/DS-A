eca3fab26b03f662de183fc58bfb876e4dea176b
snrndrkmr <snrndrkmr.gowda@gmail.com>
2019-05-16 18:47:53 +0530

    Chaining insert and search

--- src/com/hashing/Chaining.java
+++ src/com/hashing/Chaining.java
@@ -3,21 +3,20 @@ package com.hashing;
 public class Chaining {
 	public static final int CAPACITY = 100;
 	private int n;
-	private int[] slots;
+	private ListNode[] slots = null;
 	private ListNode head=null;
 	private int length=0;
-	private int index =-1;
 	private int probelength=0;
-	private int m = 3;
+	private int m = 7;
 	public Chaining() {
 		this(CAPACITY);
 	}
 	public Chaining(int cap) {
 		n = cap;
-		slots = new int[n];
+		slots = new ListNode[n];
 	}
 	public boolean isEmpty() {
-		return (index<0);
+		return (head==null);
 	}
 	public int Chainsize() {
 		return probelength;
@@ -26,12 +25,65 @@ public class Chaining {
 		return length;
 	}
 	public void insert(int data) {
-		
+		int i = data % m;
+		ListNode node = new ListNode(data);
+		node.setNext(slots[i]);
+		slots[i]=node;
+		probelength++;
+		/*if(slots[i]==null) {
+			ListNode node = new ListNode(data);
+			node.setNext(slots[i]);
+			slots[i]=node;
+		}
+		else  {
+			ListNode node = new ListNode(data);
+			node.setNext(slots[i]);
+			slots[i]=node;
+			probelength++;
+		}*/
+		length++;
 	}
-	public int search() {
-		return 0;
+	public int search(int data) {
+		int position=0;
+		int temp = data % m;
+		for(int i=0;i<=temp;i++) {
+			ListNode node = slots[i];
+				while(node!=null) {
+					if(node.getData()==data) {
+						System.out.println("data found "+node.getData());
+						}
+					node = node.getNext();
+					
+					position++;
+				}
+				position++;
+		}
+		return position;
+		
 	}
 	public int delete(int data) {
 		return 0;
+	}
+	public void traverse() {
+		for(int i=0;i<slots.length;i++) {
+			ListNode temp = slots[i];
+			while(temp!=null) {
+				System.out.print(temp.getData()+" ");
+				temp=temp.getNext();
+			}
+		}
+		System.out.println("\n");
+	}
+	public static void main(String[] args) {
+		Chaining ch = new Chaining();
+		ch.insert(25);
+		ch.insert(12);
+		ch.insert(32);
+		ch.insert(39);
+		ch.insert(40);
+		ch.traverse();
+		ch.search(12);
+		int position = ch.search(10);
+		System.out.println("data found at position "+position);
 	}
 }

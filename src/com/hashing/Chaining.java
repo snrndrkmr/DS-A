60d3a3363e01d182ef72d7c951a0a9cc2fe046bf
snrndrkmr <snrndrkmr.gowda@gmail.com>
2019-05-17 19:31:02 +0530

    chaining and Linear Probing

--- src/com/hashing/Chaining.java
+++ src/com/hashing/Chaining.java
@@ -1,5 +1,7 @@
 package com.hashing;
 
+import com.linkedList.ListNode;
+
 public class Chaining {
 	public static final int CAPACITY = 100;
 	private int n;
@@ -8,6 +10,7 @@ public class Chaining {
 	private int length=0;
 	private int probelength=0;
 	private int m = 7;
+	private int position=0;
 	public Chaining() {
 		this(CAPACITY);
 	}
@@ -26,49 +29,64 @@ public class Chaining {
 	}
 	public void insert(int data) {
 		int i = data % m;
-		ListNode node = new ListNode(data);
+		/*ListNode node = new ListNode(data);
 		node.setNext(slots[i]);
 		slots[i]=node;
-		probelength++;
-		/*if(slots[i]==null) {
-			ListNode node = new ListNode(data);
-			node.setNext(slots[i]);
-			slots[i]=node;
+		probelength++;*/
+		if(slots[i]==null) {
+			slots[i]= new ListNode(data);
+			probelength++;
 		}
 		else  {
+			ListNode temp = slots[i];
+			while(temp.getNext()!=null)
+			temp=temp.getNext();
 			ListNode node = new ListNode(data);
-			node.setNext(slots[i]);
-			slots[i]=node;
+			temp.setNext(node);
 			probelength++;
-		}*/
+		}
 		length++;
 	}
+
 	public int search(int data) {
-		int position=0;
 		int temp = data % m;
-		for(int i=0;i<=temp;i++) {
-			ListNode node = slots[i];
-				while(node!=null) {
-					if(node.getData()==data) {
-						System.out.println("data found "+node.getData());
-						}
-					node = node.getNext();
-					
-					position++;
-				}
-				position++;
-		}
-		return position;
+		ListNode node = slots[temp];
+		while(node!=null) {
+			if(node.getdata()==data) {
+				return position;
+			}
+			position++;
+			node = node.getNext();
+			}
 		
+		return Integer.MIN_VALUE;
+
 	}
-	public int delete(int data) {
-		return 0;
+
+	public void delete(int data) {
+		int temp = data % m;
+		ListNode node = slots[temp];
+		int place = search(data);
+		System.out.println(place);
+		if(place==0) {
+			ListNode node2 =slots[temp].getNext();
+			slots[temp] =node2;
+			slots[temp].setNext(null);
+		}
+		for (int i = 0; i < place - 1; i++) {
+			node = node.getNext();
+		}
+		ListNode swap = node.getNext();
+		node.setNext(swap.getNext());
+		swap.setNext(null);
+
 	}
+
 	public void traverse() {
 		for(int i=0;i<slots.length;i++) {
 			ListNode temp = slots[i];
 			while(temp!=null) {
-				System.out.print(temp.getData()+" ");
+				System.out.print(temp.getdata()+" ");
 				temp=temp.getNext();
 			}
 		}
@@ -77,13 +95,16 @@ public class Chaining {
 	public static void main(String[] args) {
 		Chaining ch = new Chaining();
 		ch.insert(25);
+		ch.insert(19);
 		ch.insert(12);
 		ch.insert(32);
 		ch.insert(39);
+		ch.insert(46);
 		ch.insert(40);
+		ch.insert(41);
 		ch.traverse();
-		ch.search(12);
-		int position = ch.search(10);
-		System.out.println("data found at position "+position);
+		/*System.out.println("data found at position "+ch.search(32));*/
+		ch.delete(32);
+		ch.traverse();
 	}
 }
--- src/com/hashing/LinearProbing.java
+++ src/com/hashing/LinearProbing.java
@@ -1,5 +1,52 @@
 package com.hashing;
 
 public class LinearProbing {
+	private final static int CAPACITY=100;
+	private int linear[];
+	private int n;
+	private int m = 7;
+	public LinearProbing() {
+		this(CAPACITY);
+	}
+	public LinearProbing(int cap) {
+		n = cap;
+		linear = new int[n];
+	}
 
+	public void insert(int data) {
+		int temp = data % m;
+		 if(temp>CAPACITY) {
+			 
+		 }
+			while(linear[temp]!=0) {
+	
+				temp = (temp+1);
+				
+			}
+			linear[temp]=data;
+			
+		
+	}
+	public void traverse() {
+		for(int i=0;i<linear.length;i++) {
+			System.out.print(linear[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		LinearProbing lp = new LinearProbing();
+		lp.insert(25);
+		lp.insert(32);
+		lp.insert(39);
+		lp.insert(46);
+		lp.insert(19);
+		lp.insert(12);
+		lp.insert(40);
+		lp.insert(41);
+		lp.insert(21);
+		lp.insert(49);
+		lp.insert(56);
+		lp.insert(63);
+		lp.insert(70);
+		lp.traverse();
+	}
 }

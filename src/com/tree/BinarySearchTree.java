ff5b9f942ddce35b6d3f7cf37d281ce7ca107901
Pc <Pc@Personal>
2019-06-04 08:24:57 +0530

    BinarySearchTree update

--- src/com/tree/BinarySearchTree.java
+++ src/com/tree/BinarySearchTree.java
@@ -7,171 +7,48 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class BinarySearchTree {
-	public void insertion(BinarySearchTreeNode root, int data) {
+	public BinarySearchTreeNode insertion(BinarySearchTreeNode root,int data) {
 		if (root == null) {
-			return;
+			BinarySearchTreeNode newNode = new BinarySearchTreeNode(data);
+			root = newNode;
+		} else if (data <= root.getData()) {
+			root.setLeft(insertion(root.getLeft(), data));
+		} else if (data > root.getData() ) {
+			root.setRight(insertion(root.getRight(), data));
 		}
-		Queue<BinarySearchTreeNode> q = new LinkedList<BinarySearchTreeNode>();
-		q.offer(root);
-		while (!q.isEmpty()) {
-			BinarySearchTreeNode temp = q.poll();
-			if (temp != null) {
-				if (temp.getData() < data) {
-					if (temp.getRight() != null) {
-						q.offer(temp.getRight());
-					} else {
-						BinarySearchTreeNode node = new BinarySearchTreeNode(data);
-						temp.setRight(node);
-					}
-				} else {
-					if (temp.getLeft() != null) {
-						q.offer(temp.getLeft());
-					} else {
-						BinarySearchTreeNode node = new BinarySearchTreeNode(data);
-						temp.setLeft(node);
-					}
-				}
-			}
-		}
+		return root;
 	}
 	public BinarySearchTreeNode search(BinarySearchTreeNode root,int data) {
 		if(root==null) {
 			return null;
 		}
-		Stack<BinarySearchTreeNode> s= new Stack<BinarySearchTreeNode>();
-		s.push(root);
-		while(!s.isEmpty()) {
-			BinarySearchTreeNode temp = s.pop();
-			if(temp!=null) {
-				if(temp.getData()==data) {
-					return temp;
-				}
-				if(temp.getData()<data) {
-					if(temp.getRight()!=null) {
-						if(temp.getRight().getData()==data) {
-							return temp.getRight();
-						}
-						s.push(temp.getRight());
-					}
-				}
-				else {
-					if(temp.getLeft()!=null) {
-						if(temp.getLeft().getData()==data) {
-							return temp.getLeft();
-						}
-						s.push(temp.getLeft());
-					}
-				}
-			}
+		else if(root.getData()==data) {
+			return root;
+		}
+		else if(data <= root.getData()) {
+			return search(root.getLeft(),data);
+		}
+		else if(data > root.getData()) {
+			return search(root.getRight(),data);
 		}
 		return root;
 	}
-	public BinarySearchTreeNode delete(BinarySearchTreeNode root, int data) {
-		
-		Stack<BinarySearchTreeNode> s = new Stack<BinarySearchTreeNode>();
-		s.push(root);
-		while(!s.isEmpty()) {
-			BinarySearchTreeNode temp = s.pop();
-			if (temp != null) {
-				if (temp.getData() == data) {
-					BinarySearchTreeNode succ = findSuccessor(root);
-					BinarySearchTreeNode pare = parentSuccesor(root);
-					temp.setData(succ.getData());
-					if (pare.getRight() == succ) {
-						pare.setRight(null);
-					}
-				}
-				if(temp.getData() > data) {
-					if(temp.getLeft()!=null) {
-						if(temp.getLeft().getData() == data) {
-							BinarySearchTreeNode succ = findSuccessor(root);
-							BinarySearchTreeNode pare = parentSuccesor(root);
-							temp.setData(succ.getData());
-							if (pare.getRight() == succ) {
-								pare.setRight(null);
-							}
-						}
-						s.push(temp.getLeft());
-					}
-					else if(temp.getRight()!=null) {
-						if(temp.getRight().getData() == data) {
-							BinarySearchTreeNode succ = findSuccessor(root);
-							BinarySearchTreeNode pare = parentSuccesor(root);
-							temp.setData(succ.getData());
-							if (pare.getRight() == succ) {
-								pare.setRight(null);
-							}
-						}
-						s.push(temp.getRight());
-					}
-				}
-			}
-		}
-		return root;
-	}
-
-	public BinarySearchTreeNode parentSuccesor(BinarySearchTreeNode root) {
-		if (root == null) {
-			return null;
-		}
-		Queue<BinarySearchTreeNode> q = new LinkedList<BinarySearchTreeNode>();
-		q.offer(root);
-		while (!q.isEmpty()) {
-			BinarySearchTreeNode temp = q.poll();
-			if (temp != null) {
-				if (temp.getRight() != null) {
-					if (temp.getRight() == findSuccessor(root)) {
-						return temp;
-					}
-					q.offer(temp.getRight());
-				}
-				if (temp.getLeft() != null) {
-					if (temp.getLeft() == findSuccessor(root)) {
-						return temp;
-					}
-					q.offer(temp.getLeft());
-				}
-			}
-		}
-		return root;
-	}
-	public BinarySearchTreeNode findSuccessor(BinarySearchTreeNode root) {
+	public BinarySearchTreeNode inordersuccessor(BinarySearchTreeNode root,int data) {
 		if(root==null) {
 			return null;
 		}
-		BinarySearchTreeNode current = root.getLeft();
-		while(current!=null) {
-			current = current.getRight();
-			return current;
+		root = search(root,data);
+		root = root.getRight();
+		while(root.getLeft()!=null) {
+			root = root.getLeft();
 		}
-		return current;
+		return root;
 	}
-	public ArrayList<Integer> preorder(BinarySearchTreeNode root) {
-		ArrayList<Integer> res = new ArrayList<Integer>();
+	public ArrayList<Integer> inorder(BinarySearchTreeNode root) {
 		if(root==null) {
-			return res;
+			return null;
 		}
-		Stack<BinarySearchTreeNode> s = new Stack<BinarySearchTreeNode>();
-		s.push(root);
-		while(!s.isEmpty()) {
-			BinarySearchTreeNode temp = s.pop();
-			if(temp!=null) {
-				res.add(temp.getData());
-				if(temp.getRight()!=null) {
-					s.push(temp.getRight());
-				}
-				if(temp.getLeft()!=null) {
-					s.push(temp.getLeft());
-				}
-			}
-		}
-		return res;
-	}
-	public ArrayList<Integer> inorder(BinarySearchTreeNode root){
 		ArrayList<Integer> res = new ArrayList<Integer>();
-		if(root==null) {
-			return res;
-		}
 		Stack<BinarySearchTreeNode> s = new Stack<BinarySearchTreeNode>();
 		BinarySearchTreeNode current = root;
 		boolean done = false;
@@ -193,44 +70,94 @@ public class BinarySearchTree {
 		}
 		return res;
 	}
-	public static void main(String[] args) {
-		BinarySearchTree t = new BinarySearchTree();
-		BinarySearchTreeNode root = new BinarySearchTreeNode(25);
-		t.insertion(root, 11);
-		t.insertion(root, 34);
-		t.insertion(root, 27);
-		t.insertion(root, 8);
-		t.insertion(root, 67);
-		t.insertion(root, 79);
-		t.insertion(root, 15);
-		/*BinarySearchTreeNode root = new BinarySearchTreeNode(1);
-		t.insertion(root, 2);
-		t.insertion(root, 3);
-		t.insertion(root, 4);
-		t.insertion(root, 5);
-		t.insertion(root, 6);
-		t.insertion(root, 7);
-		t.insertion(root, 8);*/
-		int i =34;
-		BinarySearchTreeNode var = t.search(root, i);
-		if(var.getData()==i) {
-			System.out.println("data found "+var.getData());
+	public ArrayList<Integer> preorder(BinarySearchTreeNode root) {
+		if(root==null) {
+			return null;
 		}
-		else {
-			System.out.println("data not found "+var.getData());
-		}
-		BinarySearchTreeNode succ = t.findSuccessor(root);
-		System.out.println(succ.getData());
-		BinarySearchTreeNode pare = t.parentSuccesor(root);
-		System.out.println(pare.getData());
-		
-		t.delete(root, 11);
 		ArrayList<Integer> res = new ArrayList<Integer>();
-		res = t.inorder(root);
+		Stack<BinarySearchTreeNode> s = new Stack<BinarySearchTreeNode>();
+		s.push(root);
+		while(!s.isEmpty()) {
+			BinarySearchTreeNode temp = s.pop();
+			if(temp!=null) {
+				res.add(temp.getData());
+				if(temp.getRight()!=null) {
+					s.push(temp.getRight());
+				}
+				else if(temp.getLeft()!=null) {
+					s.push(temp.getLeft());
+				}
+			}
+		}
+		return res;
+	}
+	public ArrayList<Integer> postorder(BinarySearchTreeNode root) {
+		if(root==null) {
+			return null;
+		}
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Stack<BinarySearchTreeNode> s = new Stack<BinarySearchTreeNode>();
+		s.push(root);
+		BinarySearchTreeNode prev =null;
+		while(!s.isEmpty()) {
+			BinarySearchTreeNode current = s.peek();
+			if(prev==null || prev.getRight() == current || prev.getLeft()==current) {
+				if(current.getLeft()!=null) {
+					s.push(current.getLeft());
+				}
+				else if(current.getRight()!=null) {
+					s.push(current.getRight());
+				}
+			}
+			else if(current.getLeft() == prev) {
+				if(current.getRight()!=null) {
+					s.push(current.getRight());
+				}
+			}
+			else {
+				res.add(current.getData());
+				s.pop();
+			}
+			prev = current;
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		BinarySearchTreeNode root = null;
+		BinarySearchTree t = new BinarySearchTree();
+		root = t.insertion(root, 25);
+		root = t.insertion(root, 15);
+		root = t.insertion(root, 35);
+		root = t.insertion(root, 30);
+		root = t.insertion(root, 40);
+		root = t.insertion(root, 10);
+		root = t.insertion(root, 20);
+		root = t.insertion(root, 29);
+		root = t.insertion(root, 32);
+		root = t.insertion(root, 39);
+		root = t.insertion(root, 41);
+		root = t.insertion(root, 9);
+		root = t.insertion(root, 11);
+		root = t.insertion(root, 19);
+		root = t.insertion(root, 21);
+		root = t.insertion(root, 26);
+		root = t.insertion(root, 27);
+		root = t.insertion(root, 28);
+		root = t.insertion(root, 31);
+		ArrayList<Integer> res= t.inorder(root);
 		Iterator<Integer> iter = res.iterator();
 		while(iter.hasNext()) {
 			System.out.print(iter.next()+" ");
 		}
 		System.out.println("\n");
+		int data = 25 ;
+		BinarySearchTreeNode temp = t.search(root, data);
+		if(temp.getData()==data) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		System.out.println(t.inordersuccessor(root, 25).getData());
 	}
 }

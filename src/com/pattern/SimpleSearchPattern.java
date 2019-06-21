e8b9811fb4814a4f5da43df28f2c29f38bd2a0e1
snrndrkmr <snrndrkmr.gowda@gmail.com>
2019-06-21 18:24:21 +0530

    simplesearch partial

--- src/com/pattern/SimpleSearchPattern.java
+++ src/com/pattern/SimpleSearchPattern.java
@@ -1,5 +1,47 @@
 package com.pattern;
 
-public class SimpleSearchPattern {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class SimpleSearchPattern {
+	public void Search(String string) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String sub = br.readLine();
+		char[] container = string.toCharArray();
+		char[] search = sub.toCharArray();
+		int m = search.length;
+		int n = container.length;
+		for(int i=0;i<n-m;i++) {
+			int j=0;
+			while(j<m && search[j]==container[i+j]) {
+				System.out.print(search[j]+" ");
+				System.out.print(container[i+j]+" ");
+				System.out.println();
+				j = j+1;
+			}
+			if(j==m) {
+				System.out.println("pattern found");
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		SimpleSearchPattern pt = new SimpleSearchPattern();
+		/*System.out.println("enter your age: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("enter your name");
+		sc.nextLine();
+		String str = sc.nextLine();
+		System.out.println("your age is: "+a+" and name is :"+str);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter your age: ");
+		int age = Integer.parseInt(br.readLine());
+		System.out.println("enter your name");
+		String name = br.readLine();
+		System.out.println("your age is: "+age+" and name is :"+name);*/
+		System.out.println("search here:");
+		pt.Search("narendra");
+	}
 }
